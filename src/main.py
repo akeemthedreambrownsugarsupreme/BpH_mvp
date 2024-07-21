9d1a4b80e6381778ef1127d636f1cefeced0d9b1
Habibur Rahman <bdhabib94@gmail.com>
2024-07-21 01:39:50 -0600

    Allow all origin

--- src/main.py
+++ src/main.py
@@ -1,11 +1,20 @@
 import sys
 import os
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 from src.routes import rental, commercial
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
 
 app.include_router(rental.router, prefix="/rental")
 # app.include_router(commercial.router, prefix="/commercial")
